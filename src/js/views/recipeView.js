f65b6854dd54146db11fa1b74fb401c482095d55
Supratim Nag <supratimnag1997@gmail.com>
2021-12-11 00:45:50 +0530

    Fraction not defined error fix

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,6 @@
 import icons from 'url:../../img/icons.svg';
 import View from './view';
-import { Fraction } from 'fractional/index';
+import { numberToFraction } from '../helpers';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
@@ -93,7 +93,7 @@ class RecipeView extends View {
         </svg>
         <div class="recipe__quantity">${
           ingredient.quantity
-            ? new Fraction(ingredient.quantity).toString()
+            ? numberToFraction(ingredient.quantity).toString()
             : ''
         }</div>
         <div class="recipe__description">
